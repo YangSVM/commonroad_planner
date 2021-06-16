ab2c8a646b6369ae9a76117db3b4372bd3059a6c
YangSVM <852488062@qq.com>
2021-06-16 23:26:59 +0800

    grid_lanelet.py修改并增加注释

--- grid_lanelet.py
+++ grid_lanelet.py
@@ -56,13 +56,19 @@ def lanelet_network2grid(lanelet_network):
     # 转为numpy矩阵
     lanelet_id = np.array(lanelet_id, dtype=int)
 
-    # 初始化。全部可通行
-    grid = np.zeros(lanelet_id.shape)
-
-    return grid, lanelet_id
+    return lanelet_id
 
 
-def ego_pos2tree(ego_pos, grid, lanelet_ids, lanelet_network):
+def ego_pos2tree(ego_pos,  lanelet_ids, lanelet_network):
+    '''通过自车位置，标记对应的1。并且返回自车位置相对于左上角lanelet的中心线的frenet坐标系的距离。
+    param: ego_pos:车辆位置.[x,y]
+    param: lanelet_ids:lanelet网格矩阵。每个位置为对应的lanelet_id.
+    param: lanlet_network. common-road lanelet_network
+    return: grid: 与lanelet_ids维度相同的矩阵。自车在的位置为1，其余为0
+    return: d: 自车位置相对于左上角lanelet的中心线的frenet坐标系的沿中心线方向距离
+
+    '''
+    grid = np.zeros(lanelet_ids.shape)
     lanelet_id = lanelet_network.find_lanelet_by_position([ego_pos])[0]
     for lanelet_id_i in lanelet_id:
         index = np.where(lanelet_ids == lanelet_id_i)
@@ -72,46 +78,12 @@ def ego_pos2tree(ego_pos, grid, lanelet_ids, lanelet_network):
     center_vertices = lanelet00.center_vertices
     cv, _,s_cv = detail_cv(center_vertices)
     # 找最近点
-    d = distance_lanelet(cv, s_cv, center_vertices[0,:],ego_pos)
+    s = distance_lanelet(cv, s_cv, center_vertices[0,:],ego_pos)
 
-    return grid, d
-
-
+    return grid, s
 
 
-
-
-if __name__=='__main__':
-    # 测试MCTS算法
-
-
-    #  下载 common road scenarios包。https://gitlab.lrz.de/tum-cps/commonroad-scenarios。修改为下载地址
-    path_scenario_download = os.path.abspath('/home/thicv/codes/commonroad/commonroad-scenarios/scenarios/NGSIM/US101')
-    # 文件名
-    id_scenario = 'USA_US101-16_1_T-1'
-
-    path_scenario =os.path.join(path_scenario_download, id_scenario + '.xml')
-    # read in scenario and planning problem set
-    scenario, planning_problem_set = CommonRoadFileReader(path_scenario).open()
-    # retrieve the first planning problem in the problem set
-    planning_problem = list(planning_problem_set.planning_problem_dict.values())[0]
-
-    # 可视化目前场景
-
-    # plt.figure(figsize=(25, 10))
-    # for  i in range(100):
-    #     plt.clf()
-    #     draw_parameters = {'time_begin': i}
-
-    #     draw_object(scenario, draw_params=draw_parameters)
-    #     draw_object(planning_problem_set)
-    #     plt.gca().set_aspect('equal')
-    #     plt.pause(0.01)
-
-    # 获取初始位置
-    ego_init_pos = planning_problem.initial_state.position
-
-
+def edit_scenario4test(scenario, ego_init_pos):
     # ------------ 删除车辆，使得每个车道最多一辆车 ------------------------------------
     # scenario.remove_obstacle(obstacle id)
     lanelet_network = scenario.lanelet_network
@@ -131,8 +103,26 @@ if __name__=='__main__':
         obstacle_remove.append(obstacle_id)
     for obstalce_id_remove in obstacle_remove:
         scenario.remove_obstacle(scenario.obstacle_by_id(obstalce_id_remove))
+    return scenario
 
-    # --------------------------------------------删除车辆完成。可视化 ------------------------------
+if __name__=='__main__':
+    # 测试MCTS算法
+
+    # -------------- 固定写法。从common road中读取场景 -----------------
+    #  下载 common road scenarios包。https://gitlab.lrz.de/tum-cps/commonroad-scenarios。修改为下载地址
+    path_scenario_download = os.path.abspath('/home/thicv/codes/commonroad/commonroad-scenarios/scenarios/NGSIM/US101')
+    # 文件名
+    id_scenario = 'USA_US101-16_1_T-1'
+    path_scenario =os.path.join(path_scenario_download, id_scenario + '.xml')    
+    scenario, planning_problem_set = CommonRoadFileReader(path_scenario).open()
+    planning_problem = list(planning_problem_set.planning_problem_dict.values())[0]
+    # -------------- 读取结束 -----------------
+
+    # 原有场景车辆太多。删除部分车辆
+    ego_pos_init = planning_problem.initial_state.position
+    scenario = edit_scenario4test(scenario, ego_pos_init)
+
+    # ---------------可视化修改后的场景 ------------------------------
     plt.figure(figsize=(25, 10))
     # 画一小段展示一下
     for  i in range(10):
@@ -151,11 +141,12 @@ if __name__=='__main__':
         plt.pause(0.001)
         # plt.show()
     plt.close()
+    # ---------------可视化 end ------------------------------
 
     # 提供初始状态。位于哪个lanelet，距离lanelet 末端位置
+    lanelet_network  = scenario.lanelet_network
+    lanelet_id_matrix  = lanelet_network2grid(lanelet_network)
 
-    ego_pos = planning_problem.initial_state.position
-
-    grid, lanelet_id  = lanelet_network2grid(lanelet_network)
-    grid, d =ego_pos2tree(ego_pos, grid, lanelet_id, lanelet_network)
+    # 在每次规划过程中，可能需要反复调用这个函数得到目前车辆所在的lanelet，以及相对距离
+    grid, d =ego_pos2tree(ego_pos_init, lanelet_id_matrix, lanelet_network)
     print('grid',grid,'d',d)
