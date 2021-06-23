49434a16262adaf1a0cd289a36297ea3d9d61f2b
YangSVM <852488062@qq.com>
2021-06-23 15:46:50 +0800

    Update MCTs_CR.py

--- MCTs_CR.py
+++ MCTs_CR.py
@@ -1,17 +1,13 @@
 # -*- coding: UTF-8 -*-
-# 写于0623，用于调用 MCTs_v2 以及 grid_lanelet 两个代码的接口。
-
-from typing import NewType
 from commonroad.common.file_reader import CommonRoadFileReader
+from commonroad.scenario.obstacle import Obstacle
 from commonroad.scenario.scenario import Scenario
 from commonroad.visualization.draw_dispatch_cr import draw_object
 import os
-import numpy as np
-import matplotlib.pyplot as plt
-from numpy.core.defchararray import str_len
-from intersection_planner import conf_agent_checker
 from detail_central_vertices import detail_cv
 from intersection_planner import distance_lanelet
+import numpy as np
+import matplotlib.pyplot as plt
 
 from grid_lanelet import edit_scenario4test
 from grid_lanelet import lanelet_network2grid
@@ -33,15 +29,22 @@ if __name__ == '__main__':
     ego_pos_init = planning_problem.initial_state.position
     scenario = edit_scenario4test(scenario, ego_pos_init)
     # 提供初始状态。位于哪个lanelet，距离lanelet 末端位置
-    lanelet_network = scenario.lanelet_network
-    lanelet_id_matrix = lanelet_network2grid(lanelet_network)
-    # 在每次规划过程中，可能需要反复调用这个函数得到目前车辆所在的lanelet，以及相对距离
-    grid, d = ego_pos2tree(ego_pos_init, lanelet_id_matrix, lanelet_network)
-    print('grid', grid, 'd', d)
+    lanelet_network  = scenario.lanelet_network
+    lanelet_id_matrix  = lanelet_network2grid(lanelet_network)
+    #print('lanelet_id_matrix: ', lanelet_id_matrix)
 
-    b = [2, 50, 15]
-    ini = [[80, 10], [80, 10], [0, 0], [0, 0], [0, 0]]
+    # 在每次规划过程中，可能需要反复调用这个函数得到目前车辆所在的lanelet，以及相对距离
+    T = 50           # 5*0.1=0.5.返回0.5s时周车的状态。注意下面函数返回的自车状态仍然是初始时刻的。
+    grid, ego_d, obstacle_states =ego_pos2tree(ego_pos_init, lanelet_id_matrix, lanelet_network, scenario, T)
+    print('车辆所在车道标记矩阵：',grid,'自车frenet距离', ego_d)
+    v_ego = planning_problem.initial_state.velocity
+    #print('自车初始速度： ', v_ego)
+    #print('他车矩阵', obstacle_states)
+
+    b = [1, 50, 15] #自车状态，需要外部输入（【车道，位置，车速】）
+    #ini = [[80, 10], [80, 10], [0, 0], [0, 0], [0, 0]]   （这个矩阵的含义，为各个车道障碍车的【位置，速度】）
+    ini = obstacle_states
     initialState = NaughtsAndCrossesState(b, ini)
-    searcher = mcts(iterationLimit=5000)  # 改变循环次数或者时间
+    searcher = mcts(iterationLimit=5000)  # 改变循环次数
     action = searcher.search(initialState=initialState)  # 一整个类都是其状态
-    print(action.act)
+    print(action.act)    #输出第一步的行为：1，上变道；2，下变道；3，直行加速；4，直行匀速；5，直行减速
